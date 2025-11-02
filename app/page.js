506642d8da7de63e53fd8671e091660eb10aa8ba
Joseph Man <talk@josephman.co.uk>
2025-11-02 20:54:10 +0000

    init

--- app/page.js
+++ app/page.js
@@ -1,65 +1,56 @@
+"use client";
+
 import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function Home() {
+  const pathname = usePathname();
   return (
-    <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
-      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+    <>
+      <div className="border-b border-gray-300 shadow-md h-28 px-8">
         <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
+          src="/logo.svg"
+          alt="Master Collection Cards Logo"
+          width={200}
+          height={200}
         />
-        <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
-          <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
-            To get started, edit the page.js file.
-          </h1>
-          <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+      </div>
+      <main className="flex">
+        <div className="w-1/5 h-[calc(100vh-7em)] min-h-[calc(100vh-7em)] border-r border-gray-300">
+          <nav>
+            <Link
+              href="/"
+              className={`${
+                pathname === "/"
+                  ? "bg-brand-red text-white"
+                  : "hover:bg-brand-red hover:text-white"
+              } block px-8 py-6 bg-linear-to-b hover:from-brand-red hover:to-brand-red-warm hover:text-white`}
             >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              className="font-medium text-zinc-950 dark:text-zinc-50"
+              Recommended Cards
+            </Link>
+            <Link
+              href="/collections"
+              className="block px-8 py-6 border-gray-300 border-b"
             >
-              Learning
-            </a>{" "}
-            center.
-          </p>
+              My Collections
+            </Link>
+            <Link
+              href="/decks"
+              className="block px-8 py-6 border-gray-300 border-b"
+            >
+              My Decks
+            </Link>
+          </nav>
         </div>
-        <div className="flex flex-col gap-4 text-base font-medium sm:flex-row">
-          <a
-            className="flex h-12 w-full items-center justify-center gap-2 rounded-full bg-foreground px-5 text-background transition-colors hover:bg-[#383838] dark:hover:bg-[#ccc] md:w-[158px]"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className="flex h-12 w-full items-center justify-center rounded-full border border-solid border-black/[.08] px-5 transition-colors hover:border-transparent hover:bg-black/[.04] dark:border-white/[.145] dark:hover:bg-[#1a1a1a] md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
+        <div className="w-4/5 h-[calc(100vh-7em)] min-h-[calc(100vh-7em)] overflow-scroll">
+          <div className="grid grid-cols-5">
+            <div className="border border-gray-300 p-8 border-t-0 border-l-0 h-32 flex items-center justify-center">
+              Card 1
+            </div>
+          </div>
         </div>
       </main>
-    </div>
+    </>
   );
 }
